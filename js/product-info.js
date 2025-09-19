a5fc28a8d501be1f5cd2ec0dd0ba034c760e5547
Juan José Fernández <ponisaurio079@hotmail.com>
2025-09-18 23:42:38 -0300

    fix

--- js/product-info.js
+++ js/product-info.js
@@ -5,12 +5,30 @@ const titulo = document.getElementById("listado-autos2");
     if (producto) {
       detalle.innerHTML = `
         
-        <img src="${producto.image}" alt="${producto.name}" class="img-thumbnail" style="max-width:300px">
-        <p><b>Precio:</b> ${producto.currency} ${producto.cost}</p>
-        <p><b>Vendidos:</b> ${producto.soldCount}</p>
-        <p><b>Descripción:</b> ${producto.description}</p>
+        <img src="${producto.image}" alt="${producto.name}" class="img-info" >
+        <div id="Descripciontotal"> 
+        <p id="precio"> ${producto.currency} ${producto.cost}</p>
+        
+        <p id="vendidos"><b>Vendidos:</b> ${producto.soldCount}</p>
+        <h2 id=Tdesc>Descripcion </h2>
+        <p id="descripcion"> ${producto.description}</p>
+        </div>
+        
       `;
       titulo.innerHTML = `<h2>${producto.name}</h2>`;
     } else {
       detalle.textContent = "No se encontró información del producto.";
     }
+    
+const desplegable = document.querySelector(".menu-desplegable");
+const navMenu = document.querySelector(".navbar-nav");
+
+desplegable.addEventListener("click", () => {
+    desplegable.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    desplegable.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
