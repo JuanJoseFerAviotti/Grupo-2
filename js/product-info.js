cc60bf3ac3726068fac8dae640f4fc0c61c14811
Juan José Fernández <ponisaurio079@hotmail.com>
2025-09-18 21:20:51 -0300

    fix pInfo

--- js/product-info.js
+++ js/product-info.js
@@ -1,14 +1,16 @@
 const producto = JSON.parse(localStorage.getItem("productoSeleccionado"));
 const detalle = document.getElementById("detalle");
+const titulo = document.getElementById("listado-autos2");
 
     if (producto) {
       detalle.innerHTML = `
-        <h2>${producto.name}</h2>
+        
         <img src="${producto.image}" alt="${producto.name}" class="img-thumbnail" style="max-width:300px">
         <p><b>Precio:</b> ${producto.currency} ${producto.cost}</p>
         <p><b>Vendidos:</b> ${producto.soldCount}</p>
         <p><b>Descripción:</b> ${producto.description}</p>
       `;
+      titulo.innerHTML = `<h2>${producto.name}</h2>`;
     } else {
       detalle.textContent = "No se encontró información del producto.";
     }
