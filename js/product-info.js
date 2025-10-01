f381aa6fdf2bf6c73fe7b41f6398ecebaf03de4b
maxiigoomez <maxiigoomez1995@gmail.com>
2025-10-01 09:17:46 -0300

    Agregar comentario dinamico

--- js/product-info.js
+++ js/product-info.js
@@ -5,6 +5,57 @@ const titulo2 = document.getElementById("listado-autos1");
 const productoId = producto.id;
 let productoComentarios = [];
 const url = PRODUCT_INFO_COMMENTS_URL + productoId + ".json";
+let calificacionSeleccionada = 0;
+const stars = document.querySelectorAll('.star');
+const form = document.getElementById('formCalificacion');
+
+
+stars.forEach(star => {
+    star.addEventListener('click', function() {
+        calificacionSeleccionada = parseInt(this.dataset.rating);
+        actualizarEstrellas();
+    });
+});
+
+function actualizarEstrellas() {
+    stars.forEach((star, index) => {
+        if (index < calificacionSeleccionada) {
+            star.classList.add('active');
+        } else {
+            star.classList.remove('active');
+        }
+    });
+}
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const comentario = document.getElementById('comentario').value;
+    if (calificacionSeleccionada === 0) {
+        alert('Por favor, selecciona una calificación.');
+        return;
+    }
+    if (comentario.trim() === '') {
+        alert('Por favor, escribe un comentario.');
+        return;
+    }
+    const nuevoComentario = {
+        user:localStorage.getItem("user"),
+        description: comentario,
+        score: calificacionSeleccionada,
+        dateTime: new Date().toISOString()
+    };
+    productoComentarios.push(nuevoComentario);
+    mostrarComentarios();
+    
+    alert('Comentario enviado!');
+    
+    
+    form.reset();
+    calificacionSeleccionada = 0;
+    actualizarEstrellas();
+});
 
 
     if (producto) {
@@ -43,13 +94,21 @@ function mostrarComentarios() {
     const comentarios = document.getElementById('comentarios');
   
     comentarios.innerHTML = '';
-    
+    productoComentarios.sort((a, b) => new Date(b.dateTime) - new Date(a.dateTime));
     productoComentarios.forEach(comentario => {
+        const fecha = new Date(comentario.dateTime);
+        const fechaFormateada = fecha.toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+        });
         const comentarioHTML = `
             <div class="comentario">
-                <h4 class="usuario">${comentario.user}</h4>
-                <p class="descripcion">${comentario.description}</p>
-                <p class="fecha">${comentario.dateTime}</p>
+                <h3 class="usuario">${comentario.user}</h4>
+                <label class="descripcion">${comentario.description}</label>
+                <label class="fecha">${fechaFormateada}</label>
                 <p class="puntuacion" style="text-align: end;">Puntuación: ${convertirEstrellas(comentario.score)}</p>
             </div>
             <hr>    
